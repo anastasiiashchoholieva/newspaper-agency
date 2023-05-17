755090db00cfa19202a5c315abca68c3d9e35d19
Anastasiia <nastyashch1601@gmail.com>
2023-05-17 14:08:32 +0100

    Add DetailView for Newspaper, Publisher; update pagination

--- agency/urls.py
+++ agency/urls.py
@@ -3,7 +3,9 @@ from agency.views import (
     index,
     TopicListView,
     NewspaperListView,
-    PublisherListView
+    PublisherListView,
+    NewspaperDetailView,
+    PublisherDetailView
 )
 
 urlpatterns = [
@@ -19,9 +21,19 @@ urlpatterns = [
         name="newspaper-list",
     ),
     path(
+        "articles/<int:pk>/",
+        NewspaperDetailView.as_view(),
+        name="article-detail",
+    ),
+    path(
         "publishers/",
         PublisherListView.as_view(),
         name="publisher-list",
+    ),
+    path(
+        "publishers/<int:pk>/",
+        PublisherDetailView.as_view(),
+        name="publisher-detail",
     ),
 ]
 
